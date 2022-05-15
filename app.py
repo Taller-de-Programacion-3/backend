7798a20fb59cd7bd34bb30ae705467d098d2b52d
Patricio Avigliano <patricioavigliano@gmail.com>
2022-05-15 08:47:05 -0300

    GET /tasks dummy

---
+++ app.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+from flask import Flask, request, make_response, jsonify
+from random import getrandbits
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello world'
+
+@app.route('/tasks/<device_id>', methods=['GET'])
+def get_tasks(device_id):
+    tasks = []
+    if (bool(getrandbits(1))):
+        tasks.append({ 'name': 'led_on', 'config': {} })
+    else:
+        tasks.append({ 'name': 'led_off', 'config': {} })
+    return make_response(jsonify(tasks=tasks), 200)
+
+
+if __name__ == '__main__':
+    try:
+        app.run(port=os.environ['PORT'])
+    except KeyError:
+        app.run()
