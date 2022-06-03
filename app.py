f83d87287a40bd84ead315f7126e42b1e2f16128
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2022-06-02 23:37:11 -0300

    Add db

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from flask import Flask
 from devices import devices_blueprint
 from api import api_blueprint
 
+from sqlalchemy import create_engine
+
+db_url = os.environ.get('DB_URL')
+
 app = Flask(__name__)
 
 logger = logging.getLogger()
@@ -15,8 +19,16 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 app.register_blueprint(devices_blueprint, url_prefix='/devices')
 app.register_blueprint(api_blueprint, url_prefix='/api')
 
+engine = create_engine(db_url)
+
 @app.route('/')
 def hello():
+    with engine.connect() as con:
+        query_result = con.execute('SELECT * FROM tasks')
+
+        for row in query_result:
+            print('row')
+
     return 'Hello world'
 
 
